6fea17310a2c1a76c2f258f81869593150edc356
Polina <chebanenkopolina@gmail.com>
2026-05-17 16:34:17 -0700

    managing fulfillment proofs and update retirement logic

--- app/services/retirement_service.rb
+++ app/services/retirement_service.rb
@@ -3,8 +3,8 @@ class RetirementService
     orders_mass = offset.orders.sum(:mass_g)
     proofs_mass = offset.fulfillment_proofs.sum(:mass_g)
 
-    return unless orders_mass >= offset.mass_g || proofs_mass >= offset.mass_g
+    offset.update!(retired: true) if orders_mass >= offset.mass_g && proofs_mass >= offset.mass_g
 
-    offset.update!(retired: true)
+    offset
   end
 end
